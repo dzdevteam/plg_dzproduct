dcd635310e03b3e51fd6265fc56bc1ac0d067084
Lê Hoàng Phương <herophuong93@gmail.com>
2013-08-02 16:42:06 +0000

    support field data creation on item edit form

--- content/dzproduct/dzproduct.php
+++ content/dzproduct/dzproduct.php
@@ -43,58 +43,92 @@ class PlgContentDZProduct extends JPlugin
         return true;
     }
     
-    public function onContentAfterSave($context, &$article, $isNew)
+    public function onContentAfterSave($context, $table, $isNew)
     {
-        if ($context != 'com_categories.category' && JRequest::getVar('extension') != 'com_dzproduct.items.catid')
-            return true;
-        $catid = $article->id;
+        if ($context == 'com_categories.category' && JRequest::getVar('extension') == 'com_dzproduct.items.catid') {
+            $catid = $table->id;
+            
+            $registry = new JRegistry();
+            $registry->loadString($table->params);
+            $params = $registry->toArray();
+            $groupid = $params['groupid'];
+            
+            $relation_table = JTable::getInstance('Relation', 'DZProductTable');
+            if ($relation_table->load(array('catid' => $table->id)))
+                $id = $relation_table->id;
+            else
+                $id = 0;
+            
+            $data = array('id' => $id, 'catid' => $catid, 'groupid' => $groupid);
+            $relation_model = JModelLegacy::getInstance('Relation', 'DZProductModel');
+            
+            // Get the form
+            $form = $relation_model->getForm($data, false);
+            if (!$form)
+                return false;
+            
+            // Test whether the data is valid.
+            $validData = $relation_model->validate($form, $data);
+            if ($validData)
+                $relation_model->save($validData);
+            else
+                return false;
+        }
         
-        $registry = new JRegistry();
-        $registry->loadString($article->params);
-        $params = $registry->toArray();
-        $groupid = $params['groupid'];
-        
-        $relation_table = JTable::getInstance('Relation', 'DZProductTable');
-        if ($relation_table->load(array('catid' => $article->id)))
-            $id = $relation_table->id;
-        else
-            $id = 0;
-        
-        $data = array('id' => $id, 'catid' => $catid, 'groupid' => $groupid);
-        $relation_model = JModelLegacy::getInstance('Relation', 'DZProductModel');
-        
-        // Get the form
-        $form = $relation_model->getForm($data, false);
-        if (!$form)
-            return false;
-        
-        // Test whether the data is valid.
-        $validData = $relation_model->validate($form, $data);
-        if ($validData)
-            $relation_model->save($validData);
-        else
-            return false;
-        
+        if ($context == 'com_dzproduct.item') {
+            $input = JFactory::getApplication()->input->getArray(array("jform" => array("fields" => 'array')));
+            $fields = $input['jform']['fields'];
+            $itemid = $table->id;
+            foreach ($fields as $fieldid => $value) {
+                $fielddata_table = JTable::getInstance('FieldData', 'DZProductTable');
+                if ($fielddata_table->load(array('fieldid' => $fieldid, 'itemid' => $itemid)))
+                    $id = $fielddata_table->id;
+                else 
+                    $id = 0;
+                
+                $fielddata_model = JModelLegacy::getInstance('FieldData', 'DZProductModel');
+                $data = array('id' => $id, 'itemid' => $itemid, 'fieldid' => $fieldid, 'value' => $value);
+                // Get the form
+                $form = $fielddata_model->getForm($data, false);
+                if (!$form)
+                    continue;
+                
+                // Test whether the data is valid.
+                $validData = $fielddata_model->validate($form, $data);
+                if ($validData)
+                    $fielddata_model->save($validData);
+                else
+                    continue;
+            }
+        }
         return true;
     }
     
-    public function onContentAfterDelete($context, $article)
+    public function onContentAfterDelete($context, $table)
     {
-        if ($context != 'com_categories.category' && JRequest::getVar('extension') != 'com_dzproduct.items.catid')
-            return true;
-        $catid = $article->id;
-        
-        $relation_table = JTable::getInstance('Relation', 'DZProductTable');
-        if ($relation_table->load(array('catid' => $article->id)))
-            $id = $relation_table->id;
-        else
-            $id = 0;
-        
-        if ($id) {
-            $relation_model = JModelLegacy::getInstance('Relation', 'DZProductModel');
-            $relation_model->delete($id);
+        // Delete all relations associated to a category if it is deleted
+        if ($context == 'com_categories.category' && JRequest::getVar('extension') == 'com_dzproduct.items.catid') {
+            $catid = $table->id;
+            
+            $relation_table = JTable::getInstance('Relation', 'DZProductTable');
+            if ($relation_table->load(array('catid' => $catid))) {
+                $id = $relation_table->id;
+                $relation_model = JModelLegacy::getInstance('Relation', 'DZProductModel');
+                $relation_model->delete($id);
+            }
         }
         
+        // Delete all field data associated to an item if it is deleted
+        if ($context == 'com_dzproduct.item') {
+            $itemid = $table->id;
+            
+            $fielddata_table = JTable::getInstance('FieldData', 'DZProductTable');
+            while ($fielddata_table->load(array('itemid' => $itemid))) {
+                $id = $fielddata_table->id;
+                $fielddata_model = JModelLegacy::getInstance('FieldData', 'DZProductModel');
+                $fielddata_model->delete($id);
+            }
+        }
         return true;
     }
 }
